e27ea96641f6aae476443909ffd56d8043784546
Fritzss <Sergey.Nik.Krylov@gmail.com>
2021-06-29 07:25:55 +0300

    Update check_ssl.py

--- check_SSL/check_ssl.py
+++ check_SSL/check_ssl.py
@@ -4,13 +4,11 @@ import datetime
 import socket
 import argparse
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--urls', nargs='+', dest='urls', type=str)
 args = parser.parse_args()
-
-tt = ''
 tt = datetime.datetime.now()
+
 
 def ssl_expiry_datetime(host, tt, port=443):
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
@@ -18,29 +16,29 @@ def ssl_expiry_datetime(host, tt, port=443):
     conn = context.wrap_socket(
         socket.socket(socket.AF_INET),
         server_hostname=host,
-        )
+    )
     # 3 second timeout because Lambda has runtime limitations
     conn.settimeout(3.0)
     try:
-       conn.connect((host, port))
-       ssl_info = conn.getpeercert()
-    # parse the string from the certificate into a Python datetime object
-       res = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
-       expiry = (res - tt).days
+        conn.connect((host, port))
+        ssl_info = conn.getpeercert()
+        # parse the string from the certificate into a Python datetime object
+        res = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+        expiry = (res - tt).days
     except Exception as e:
         res = e
         expiry = '100'
     return res, expiry
 
-zabbixurls = '{\n"data":[\n'
 
-for url in  args.urls:
-     try:
-         check = ssl_expiry_datetime(url, tt)
-     except Exception as e:
-         check = e
-     zabbixurls += '{\n"URL": "' + url + '", ' + '\n"EXDATE": "' + str(check[0]).split(' ')[0] +'",' + '\n"EXPIRY": ' + str(check[1]) + '\n},'
+zabbixurls = '{\n"data":[\n'
+for url in args.urls:
+    try:
+        check = ssl_expiry_datetime(url, tt)
+    except Exception as e:
+        check = e
+    zabbixurls += '{\n"URL": "' + url + '", ' + '\n"EXDATE": "' + str(check[0]).split(' ')[
+        0] + '",' + '\n"EXPIRY": ' + str(check[1]) + '\n},'
 zabbixurls = zabbixurls[:-1]
 zabbixurls += '\n]\n}'
-
 print(zabbixurls)
