4bf22e334e7c33439e7d8685fba0ee276e178c5c
Fritzss <Sergey.Nik.Krylov@gmail.com>
2025-08-20 20:40:45 +0300

    Update check_ssl.py

--- check_SSL/check_ssl.py
+++ check_SSL/check_ssl.py
@@ -3,42 +3,42 @@ import ssl
 import datetime
 import socket
 import argparse
+import json
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--urls', nargs='+', dest='urls', type=str)
-args = parser.parse_args()
-tt = datetime.datetime.now()
-
-
-def ssl_expiry_datetime(host, tt, port=443):
+def ssl_expiry_datetime(host, port=443):
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
     context = ssl.create_default_context()
+    context.minimum_version = ssl.TLSVersion.TLSv1_2
     conn = context.wrap_socket(
         socket.socket(socket.AF_INET),
         server_hostname=host,
     )
-    # 3 second timeout because Lambda has runtime limitations
     conn.settimeout(3.0)
     try:
         conn.connect((host, port))
         ssl_info = conn.getpeercert()
-        # parse the string from the certificate into a Python datetime object
-        res = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
-        expiry = (res - tt).days
+        expiry_date = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+        expiry_days = (expiry_date - datetime.datetime.now()).days
+        return expiry_date, expiry_days
     except Exception as e:
-        res = e
-        expiry = '100'
-    return res, expiry
+        return None, str(e)
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--urls', nargs='+', required=True, help="Список URLs для проверки")
+    args = parser.parse_args()
+    
+    results = []
+    for url in args.urls:
+        expiry_date, expiry_days = ssl_expiry_datetime(url)
+        result = {
+            "URL": url,
+            "EXDATE": expiry_date.strftime("%Y-%m-%d") if expiry_date else "ERROR",
+            "EXPIRY": expiry_days if isinstance(expiry_days, int) else -1  # -1 для ошибок
+        }
+        results.append(result)
+    
+    print(json.dumps({"data": results}, indent=2))
 
-zabbixurls = '{\n"data":[\n'
-for url in args.urls:
-    try:
-        check = ssl_expiry_datetime(url, tt)
-    except Exception as e:
-        check = e
-    zabbixurls += '{\n"URL": "' + url + '", ' + '\n"EXDATE": "' + str(check[0]).split(' ')[
-        0] + '",' + '\n"EXPIRY": ' + str(check[1]) + '\n},'
-zabbixurls = zabbixurls[:-1]
-zabbixurls += '\n]\n}'
-print(zabbixurls)
+if __name__ == "__main__":
+    main()
